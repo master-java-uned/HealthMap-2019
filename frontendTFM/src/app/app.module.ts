329689d02a617e597c4c5d357ed87d38f25e25d4
yeraym2 <ychull@hotmail.com>
2019-12-06 11:35:54 +0000

    Actualización

--- frontendTFM/src/app/app.module.ts
+++ frontendTFM/src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import{LoginComponent} from './login/login.component'
+import { LoginComponent } from './login/login.component'
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
 import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from './user/user.component';
-import{interceptorProvider} from './interceptors/rejilla-interceptor.service';
+import { interceptorProvider } from './interceptors/rejilla-interceptor.service';
 import { MostrarRejillaComponent } from './mostrar-rejilla/mostrar-rejilla.component';
 import { ConstructosComponent } from './constructos/constructos.component'
 @NgModule({
--- frontendTFM/src/app/mostrar-rejilla/mostrar-rejilla.component.ts
+++ frontendTFM/src/app/mostrar-rejilla/mostrar-rejilla.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mostrar-rejilla',
-  templateUrl: './mostrar-rejilla.component.html',
-  styleUrls: ['./mostrar-rejilla.component.css']
+   selector: 'app-mostrar-rejilla',
+   templateUrl: './mostrar-rejilla.component.html',
+   styleUrls: ['./mostrar-rejilla.component.css']
 })
 export class MostrarRejillaComponent implements OnInit {
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
